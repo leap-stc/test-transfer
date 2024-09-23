313de91a46d53f5ba2fd7d4c82380c605fb5327c
Raphael Hagen <norlandrhagen@gmail.com>
2024-09-23 13:55:57 -0600

    modify dataflow config

--- configs/config_dataflow.py
+++ configs/config_dataflow.py
@@ -4,10 +4,10 @@ import os
 repo_path = os.environ['GITHUB_REPOSITORY']
 FEEDSTOCK_NAME = repo_path.split('/')[-1]
 
-c.Bake.prune = 1
+c.Bake.prune = False
 c.Bake.bakery_class = "pangeo_forge_runner.bakery.dataflow.DataflowBakery"
 c.DataflowBakery.use_dataflow_prime = True
-c.DataflowBakery.max_workers = 50
+c.DataflowBakery.max_workers = 1
 c.DataflowBakery.use_public_ips = True
 c.DataflowBakery.service_account_email = (
     "leap-community-bakery@leap-pangeo.iam.gserviceaccount.com"
