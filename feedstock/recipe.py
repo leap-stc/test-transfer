6df8aaec7f23f534b59ba741bf9fd38a30038299
Julius Busecke <julius@ldeo.columbia.edu>
2024-09-26 18:58:15 -0400

    this seems silly

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -54,8 +54,7 @@ class Transfer(beam.PTransform):
         target_bucket = f"m2lines-test"
         # source_prefix = "data-library/feedstocks/GODAS"
         source_prefix = ""
-        # target_prefix = "test-transfer-beam-clean-2workers"
-        target_prefix = "test-transfer-500GB-faster-more-mooooore"
+        target_prefix = "test-transfer-500GB-crazy-town"
 
         # construct full valid rclone 'paths'
         source = os.path.join(source_bucket, source_prefix, store_name).strip('/')
@@ -64,7 +63,7 @@ class Transfer(beam.PTransform):
         logger.warning(f"Copying from {source} to {target}")
   
         copy_proc = subprocess.run(
-            f'rclone copy --transfers 128 --checkers 256 -vv -P "{gcs_remote}{source}/" "{osn_remote}{target}/"',
+            f'rclone copy --transfers 256 --checkers 512 -vv -P "{gcs_remote}{source}/" "{osn_remote}{target}/"',
             shell=True,
             capture_output=False, # will expose secrets if true
             text=True, 
